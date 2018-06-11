419cfedf45beef1e87ccffcb40871c9bf8918021
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-06-11 18:35:24 -0400

    savepoint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,16 +7,17 @@ $(document).ready(function() {
     var fightOn = false;
     var fighter = null;
     var opponent = null;
+    var round = 0;
     
     // characters
     
-    var rey = {
+    var objRey = {
         HP: 100,
         initialHP: 100,
         attackPower: 10,
         counterPower: 10,
         isAlive: true,
-        select: $(".rey"),
+        select: $("#rey"),
         dead: function() {
             this.isAlive = false;
             this.select.addClass("dead");
@@ -26,15 +27,31 @@ $(document).ready(function() {
             this.isAlive = true;
             this.select.removeClass("dead");
         },
+        updateHP: function() {
+            $("#rey > .span-stat").html(this.HP);
+        },
+        attack: function() {
+            if(opponent != null) {
+                opponent.HP -= this.attackPower * round;
+                opponent.updateHP();
+            }
+        },
+        counterAttack: function() {
+            if(fighter != null) {
+                fighter.HP -= this.counterPower;
+                fighter.updateHP();
+            }
+        }
     }
 
-    var snoke = {
+    var objSnoke = {
         HP: 100,
         initialHP: 100,
         attackPower: 10,
         counterPower: 10,
         isAlive: true,
         select: $(".snoke"),
+        selectHP: $("#snoke > .span-stat"),
         dead: function() {
             this.isAlive = false;
             this.select.addClass("dead");
@@ -44,9 +61,12 @@ $(document).ready(function() {
             this.isAlive = true;
             this.select.removeClass("dead");
         },
+        updateHP: function() {
+            $("#snoke > .span-stat").html(this.HP);
+        }
     }
 
-    var kylo = {
+    var objKylo = {
         HP: 100,
         initialHP: 100,
         attackPower: 10,
@@ -62,9 +82,12 @@ $(document).ready(function() {
             this.isAlive = true;
             this.select.removeClass("dead");
         },
+        updateHP: function() {
+            $("#kylo > .span-stat").html(this.HP);
+        }
     }
 
-    var poe = {
+    var objPoe = {
         HP: 100,
         initialHP: 100,
         attackPower: 10,
@@ -80,6 +103,9 @@ $(document).ready(function() {
             this.isAlive = true;
             this.select.removeClass("dead");
         },
+        updateHP: function() {
+            $("#poe > .span-stat").html(this.HP);
+        }
     }
 
     // global functions
@@ -89,16 +115,15 @@ $(document).ready(function() {
 
         // let player select fighter
 
-        $(".character").addClass("select-time");
+        $(".character").addClass("selectable");
         selectFighterOn = true;
-        newRound();
     }
 
     function newOpponent() {
 
         // let player select opponent
 
-        $(".character").addClass("select-time");
+        $(".character").addClass("selectable");
         selectOpponentOn = true;
     }
 
@@ -109,6 +134,23 @@ $(document).ready(function() {
         
         if(selectFighterOn == true) {
 
+            // assign fighter var
+            switch($(this).attr("id")) {
+                case "rey":
+                    fighter = objRey;
+                    alert("this worked");
+                    break;
+                case "snoke":
+                    fighter = objSnoke;
+                    break;
+                case "kylo":
+                    fighter = objKylo;
+                    break;
+                case "poe":
+                    fighter = objPoe;
+                    break;
+            }
+            
             // move fighter to position
             $(this).appendTo("#div-fighter");
             $(this).addClass("fighter");
@@ -116,9 +158,28 @@ $(document).ready(function() {
             selectOpponentOn = true;
 
         } else if(selectOpponentOn == true) {
+            
+            // assign opponent var
+            switch($(this).attr("id")) {
+                case "rey":
+                    opponent = objRey;
+                    alert("this worked");
+                    break;
+                case "snoke":
+                    opponent = objSnoke;
+                    break;
+                case "kylo":
+                    opponent = objKylo;
+                    break;
+                case "poe":
+                    opponent = objPoe;
+                    break;
+            }
+            
+            // move opponent to position
             $(this).appendTo("#div-opponent");
             selectOpponentOn = false;
-            $(".character").removeClass("select-time");
+            $(".character").removeClass("selectable");
             fightOn = true;
         }
         
@@ -128,9 +189,9 @@ $(document).ready(function() {
 
         if(fightOn == true) {
             $(".fighter").animate({left:"+=30"}, 200).animate({left:"-=40"}, 200).animate({left:"+=10"});
-            $("#div-healthBar").animate({width:250}, 2000);
+            var b = "40%";
+            $("#div-healthBar").animate({width:b}, 1000);
         }
-        
     })
 
 
@@ -139,10 +200,9 @@ $(document).ready(function() {
     newGame();
 
 
-    //     $(".div-selectionOverlay").addClass("showOverlay");
-    //     $(".fade").addClass("fadeMain");
-    
-    
 
+    // overlay
+    // $(".fade").addClass("fadeMain");
+    // $(".div-selectionOverlay").addClass("showOverlay").animate({opacity:1});
 })
 
